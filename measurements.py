418f76814aba4ad20a26efc23a048b95777528de
Victor <139719863+victor0989@users.noreply.github.com>
2023-10-08 00:44:34 +0200

    Create measurements.py

---
+++ measurements.py
@@ -0,0 +1,6 @@
+brief = {'rwyelevation_m':0, 'groundrun_m':313,
+         'stloadfactor': 1.5, 'turnalt_m': 1000, 'turnspeed_ktas': 100,
+         'climbalt_m': 0, 'climbspeed_kias': 101, 'climbrate_fpm': 1398,
+         'cruisealt_m': 3048, 'cruisespeed_ktas': 182, 'cruisethrustfact': 1.0,
+         'servceil_m': 6580, 'secclimbspd_kias': 92,
+         'vstallclean_kcas': 69}
