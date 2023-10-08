f2705f94dc2701fa8e5ac50ae8fc32c87509dfe3
Victor <139719863+victor0989@users.noreply.github.com>
2023-10-08 23:57:08 +0200

    Create TESTING.bash

---
+++ TESTING.bash
@@ -0,0 +1,7 @@
+#1 testing with pytest or python + file
+#2 testing with actions-then-python-package files
+#3 automation testing with ROBOT RPA
+#4 robocorp structural-points, ideas
+#5 strace testing the robotic and sensors
+#GDB or PBD
+#More ideas are comming soon..
