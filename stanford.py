bff7aedb24545377fb2e3317aaf36d794bc19612
Victor <139719863+victor0989@users.noreply.github.com>
2023-10-08 23:53:37 +0200

    Update stanford.py

--- stanford.py
+++ stanford.py
@@ -1,7 +1,9 @@
 #INSTRUCTIONS
 #Aircraft design -stanford labs builtings
 #scikit aero scripts
+#Git-labs
 #robotic sensors in c++ for airplanes
 #modern-robotics draws and explanations for github
 #Project air-robots
 #Electric motors in python for airplanes with higher order dimensions
+#Robotics repository
